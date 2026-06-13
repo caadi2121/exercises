d024725b2c301d75d3774a2f39d7cb22a350f693
Mohammed <caadi2121@gmail.com>
2026-06-13 11:50:32 +0300

    javascript

---
+++ exercise11/index.js
@@ -0,0 +1,11 @@
+console.log("kayar <");
+for (let b = 2; b <= 50; b++) {
+  console.log(b);
+}
+
+console.log("kaweyn >");
+
+for (let d = 50; d > 0; d--) {
+  console.log(d);
+}
+// labada kayar iyo kawayn waa isticmaalay
