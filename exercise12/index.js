50b7de9ac7c19a139075f25d70522868bf822b44
Mohammed <caadi2121@gmail.com>
2026-06-13 13:00:31 +0300

    javascript

---
+++ exercise12/index.js
@@ -0,0 +1,38 @@
+console.log("objects");
+books = [
+  {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    year: 1937,
+  },
+  {
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    year: 1813,
+  },
+  {
+    title: "Harry Potter and the Philosopher's Stone",
+    author: "J.K. Rowling",
+    year: 1997,
+  },
+  {
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    year: 1988,
+  },
+  {
+    title: "A Tale of Two Cities",
+    author: "Charles Dickens",
+    year: 1859,
+  },
+];
+for (let i = 0; i < books.length; i++) {
+  console.log(books[i]);
+}
+
+console.log("<arrays>");
+
+let fruits = ["banana", "mango", "chery", "ananas"];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
